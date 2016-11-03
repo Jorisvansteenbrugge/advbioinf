56619092ab76768f1cc4ba36d24f79d375fd32a5
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2016-11-03 17:10:46 +0100

    wip

--- rosalind/prob10/script.py
+++ rosalind/prob10/script.py
@@ -1,67 +1,90 @@
 #!/usr/bin/env python
+
 """
-Author: Joris van Steenbrugge
-Function: returns the reverse complement of a DNA sequence
+Author: Joris van Steenbrugge & D.E. Bug ;)
 """
+
 from sys import argv
+import subprocess
+import re
 
+def parse_fasta(lines):
+    """Return dictionary of {label:dna_seq}
+    
+    lines: list of lines in FASTA format
+    """
+    seqs = {}
+    for line in lines:
+        if not line.strip():
+            continue
+        if line.startswith('>'):
+            label = line.strip()[0:]
+            seqs[label] = ""
+        else:
+            seqs[label] += line.strip()[0:]
+    return seqs
 
+def getRevComp(dna):
+    """
+    dna = Dictionary of {label: dnaseq}
+    Returns the reverse compliment of the sequence in a dictionary of {label: revComp}
+    """
 
+    switcher = {"A":"T", "T":"A", "G":"C", "C":"G"}
+    dna_revcomp = {}
+    for (i,y) in dna.items():
+        comp = "".join([switcher[x] for x in y])
+        dna_revcomp[i] = comp[::-1]
+    
+    return dna_revcomp
 
+def extract_kmers(seqs, k=15, skip_unknown=True):
+    """
+    Return dict of {kmer_seq: kmer_count}
+    seqs: dict of {label:dna_seq}
+    k: int, specifying k-mer size
+    skip_unknown: bool, specifying wheter k-mers containing non-TGAC characters
+        should be skipped
+    """
+    kmer_size = k
+    ch = {} #dict to store characters
+    res = [] #list to store k-mers
+    for label, seq in seqs.items():
+        for c in seq:
+            if c not in ch:
+                ch[c] = 0
+            ch[c] += 1
+        for i in range(len(seq)-kmer_size+1):
+            kmer = seq[i:i+kmer_size]
+            count = True
+            for c in kmer:
+                if c not in "TGAC":
+                    count = False
+            if skip_unknown is True and count is False:
+                continue
+            res.append(kmer)
+    return res
 
+def revPalingdrome(kmer, dna_seqs):
+    for (i,seq) in dna_seqs.items():
+        match = r'(?=(' + re.escape(kmer) + r'))'
+        matches = re.finditer(match, seq)
+        [print(str(i.start()+1)+" "+str(len(kmer))) for i in matches]
+        
 
-def getCom(dna):
-	switcher = {"A": "T", "T":"A", "G":"C","C":"G"}
-	complement = ""
-	for nuc in dna:
-		complement += switcher[nuc]
-	return complement
-
-def parseSequences(fileName):
-        handle = open(fileName)
-
-        ID = ""
-        seq = ""
-        for line in handle:
-                line = line.strip()
-                if line.startswith(">"):
-                        if seq != "":
-                                contents[calcGC(seq)] = ID
-                        ID = line.replace(">","").split()[0]
-                        seq = ""
-
-                else:
-                        seq+=line
-        return seq
-def getSubStrings(dna, dna_comp):
-	#possible sizes defined in the excercise
-	lengths = [4,5,6,7,8,9,10,11,12]
-	for n in lengths:
-		al = []
-		#copy of dna that gets modified
-		workingList = list(dna)
-		while len(workingList) >= n:
-			#Gets the n sized chunks of the string (non overlapping)
-			kmers = [workingList[i:i+n] for i in range(0, len(workingList), n)]
-			
-			#last line is often not of n-size
-			if kmers[-1] != n:
-				kmers.pop(-1)
-			for kmer in kmers:
-				kmer = "".join(kmer)
-				if getCom(kmer) in dna_comp:
-					print(getCom(kmer))
-					al.append(kmer)
-			workingList.pop(0)
-		print(list(set(al)))
-		break
 if __name__ == "__main__":
-	fileName = argv[1]
-	dna = parseSequences(fileName)
-	dna_comp = getCom(dna)
 
-	dna_revcomp = dna_comp[::-1]
-	print(dna_comp)
-	print(dna)
-	#print(dna_revcomp)
-	getSubStrings(tuple(dna), dna_comp)
+    # parse input data
+    inp_fn = argv[1]
+    dna_seqs = parse_fasta(open(inp_fn))
+    revcomp = getRevComp(dna_seqs)
+    assert type(dna_seqs) == dict , "Seq is no dict"
+    
+    #Extract k-mers  
+    for kmerSize in [4,5,6,7,8,9,10,11,12]:
+        kmer_len = kmerSize
+        kmers = extract_kmers(revcomp, skip_unknown=True, k=kmer_len)
+        assert type(kmers) == list, "kmers is no list"
+        
+        for kmer in set(kmers):
+            revPalingdrome(kmer, dna_seqs)	
