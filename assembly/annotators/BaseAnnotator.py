38545df531a393d1d25e71f5770b728d5a460ac7
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2016-11-15 22:03:05 +0100

    added functionality

--- assembly/annotators/BaseAnnotator.py
+++ assembly/annotators/BaseAnnotator.py
@@ -1,4 +1,24 @@
 
+import subprocess as sp
 
 class BaseAnnotator(object):
+    tool_index = {1: "trnascan"
+                }
+
+    def __init__(self, tool_num, in_file, out_file):
+        self.tool = self.tool_index[tool_num]
+        self.in_file = in_filie
+        self.out_file = out_file
+
+
+
+    def annotate(self):
+        cmd_list = []
+        if self.tool == "trnascan":
+            trnascan = "trnascan-1.4 {} -o {}".format(self.in_file, self.out_file)
+            cmd_list.append(trnascan)
+
+
+        for command in cmd_list:
+            sp.call(command, shell = True)
 
