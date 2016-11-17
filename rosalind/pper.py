d31bc215761cdd78ab6b1ebd39c1191fd2ea2976
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2016-11-17 13:00:36 +0100

    works but slow

--- rosalind/pper.py
+++ rosalind/pper.py
@@ -6,13 +6,10 @@ from sys import argv
 n = int(argv[1])
 k = int(argv[2])
 
-
-n_list = "".join([str(x) for x in range(n)])
-
-
+ran = [x for x in range(n)]
 count = 0
-for i in range(len(n_list)-k+1):
-    count +=1
+for i in itertools.permutations(ran, k):
+    count +=1 
 
+print(count % 1000000)
 
-print count
