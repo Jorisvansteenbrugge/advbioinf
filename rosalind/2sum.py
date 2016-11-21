818c7130c7348e886e705b2b7f54f9743494432e
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2016-11-21 11:16:48 +0100

    wip

--- rosalind/2sum.py
+++ rosalind/2sum.py
@@ -1,18 +1,41 @@
-from sys import argv
+'''
+2sum problem
+Ian Laurain
 
-def parseFile(file_name):
-    with open(file_name) as in_file:
-        arrays = []
-        in_file.readline() #skip
-        arrays = [line.strip().split() for line in in_file]
-    return arrays
+This program, given a list of integer arrays, returns
+two different indices if the values at those indices
+sum to 0.
+'''
+import sys
+from collections import defaultdict
 
 
+def two_sum(A):
+    '''
+    Takes an array A and returns the two
+    indices, if any, if the integer values
+    stored at those indices sum to 0
+    '''
+    two_sums = []
+    indices = defaultdict(list)
+    for idx in xrange(len(A)):
+        item = A[idx]
+        if -item in indices:
+            two_sum = (indices[-item][0], idx+1)
+            two_sums.append(two_sum)
+        indices[item].append(idx+1)
+    if len(two_sums) is 0:
+        return -1
+    return two_sums
 
-def get2Sum(array):
-    for val in array:
-        if "-"+val in array:
-            print val
 
-arrays = parseFile(argv[1])
-get2Sum(arrays[1])
+if __name__ == "__main__":
+    filename = sys.argv[-1]
+    with open(filename) as f:
+        arrays = [map(int, line.strip().split()) for line in f.readlines()][1:]
+        for array in arrays:
+            two_sums = two_sum(array)
+            if two_sums is -1:
+                print -1
+            else:
+                print two_sums[0][0], two_sums[0][1]
