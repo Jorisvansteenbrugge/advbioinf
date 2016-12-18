e6fd9f0feb814b21be92430b27767fadcddd1583
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2016-12-18 17:10:57 +0100

    wip

--- rosalind/classroom/prob2/script.py
+++ rosalind/classroom/prob2/script.py
@@ -4,18 +4,19 @@ from sys import argv
 
 
 def getNucString(fileName):
-	with open(fileName) as infile:
-		return infile.readline().strip()
+    with open(fileName) as infile:
+        return infile.readline().strip()
 
 def getCounts(nucString):
-	ca = str(nucString.count("A"))
-	cc = str(nucString.count("C"))
-	cg = str(nucString.count("G"))
-	ct = str(nucString.count("T"))
+    ca = str(nucString.count("A"))
+    cc = str(nucString.count("C"))
+    cg = str(nucString.count("G"))
+    ct = str(nucString.count("T"))
 
-	print("{} {} {} {}".format(ca,cc,cg,ct))
+    return "{} {} {} {}".format(ca,cc,cg,ct)
 if __name__=="__main__":
-	fileName = argv[1]
-	nucString = getNucString(fileName)
-	getCounts(nucString)
+    fileName = argv[1]
+    nuc_string = getNucString(fileName)
+    print nuc_string
+    print getCounts(nuc_string)
 
