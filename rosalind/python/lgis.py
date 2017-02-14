10a54ee2cb6773cfa1f43eae1cc94c5cafd47657
jorisvansteenbrugge <joris.vansteenbrugge@wur.nl>
2017-02-14 18:31:32 +0100

    wip

--- rosalind/python/lgis.py
+++ rosalind/python/lgis.py
@@ -10,11 +10,14 @@ def parse_infile(file_name):
     return map(int, sequence.split())
 
 
+def get_combinations(subset):
+    
+
 def get_ordered_subseqs(seq):
     s_size = len(seq)
     for i in range(s_size):
-        print(seq[i:])
-
+        rest = seq[i:]
+        
 if __name__ == "__main__":
     seq = parse_infile(argv[1])
     assert type(seq) == list, "Seq is not a list"
