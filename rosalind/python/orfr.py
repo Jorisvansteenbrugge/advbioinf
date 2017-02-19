31011ddc0975c7e57242d8da3cf74f2d68972d0b
Joris van Steenbrugge <joris.vansteenbrugge@wur.nl>
2017-02-19 11:09:03 +0100

    wip

--- rosalind/python/orfr.py
+++ rosalind/python/orfr.py
@@ -2,25 +2,6 @@
 
 from sys import argv
 
-codontable = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
-    'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-    'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-    'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W',
-    }
-
 def get_seq(file_name):
     with open(file_name) as in_file:
         seq = ""
@@ -30,28 +11,16 @@ def get_seq(file_name):
     return seq
 
 
-def read_frame(seq_frame):
-    triplets = [seq_frame[x:x+3] for x in range(0,len(seq_frame), 3)] 
-    amino = ""
-    aminos = []   
-    read = False
-    for trip in triplets:
-        if trip == "ATG":
-            read = True 
-        elif trip == "TAA" or trip == "TAG" or trip == "TGA":
-            read = False
-            if amino != "":
-                aminos.append(amino)
-            amino = ""
-        if read:
-            amino += codontable[trip]
-    return aminos
-
-
-if __name__ == "__main__":
-    seq = get_seq(argv[1])
-
-    print read_frame(seq[0:])
-    print read_frame(seq[1:])
-    print read_frame(seq[2:])
-
+from Bio import Seq
+import regex as re
+startP = re.compile('ATG')
+nuc = get_seq(argv[1])
+longest = (0,)
+for m in startP.finditer(nuc, overlapped=True):
+    if len(Seq.Seq(nuc)[m.start():].translate(to_stop=True)) > longest[0]:
+        pro = Seq.Seq(nuc)[m.start():].translate(to_stop=True)
+        longest = (len(pro), 
+                   m.start(), 
+                   str(pro),
+                   nuc[m.start():m.start()+len(pro)*3+3])
+print(longest)
