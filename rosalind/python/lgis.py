1c6f3927a658bece6099fd46e3beba2e4d7b30b1
Joris van Steenbrugge <joris.vansteenbrugge@wur.nl>
2017-02-19 10:13:24 +0100

    wip

--- rosalind/python/lgis.py
+++ rosalind/python/lgis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from sys import argv
+from itertools import permutations
 
 def parse_infile(file_name):
     with open(file_name) as in_file:
@@ -9,17 +10,32 @@ def parse_infile(file_name):
 
     return map(int, sequence.split())
 
+def get_ordered_subseqs(nums):
+    """Given a list of numbers, find the longest increasing sequence"""
+    L = [None for i in xrange(len(nums))]
+    P = [None for i in xrange(len(nums))]
+    for i in xrange(len(nums)):
+        L[i] = 1  # longest increasing seq that ends in L[i]
+        P[i] = -1 # pointer to reconstruct the sequence
+        for j in xrange(i):
+            if nums[j] < nums[i] and L[j] + 1 > L[i]:
+                P[i] = j
+                L[i] = L[j] + 1
+    index = L.index(max(L))
+    seq = [nums[index]]
+    while P[index] > -1:
+        index = P[index]
+        seq.append(nums[index])
+    seq.reverse()
+    return seq
 
-def get_combinations(subset):
-    
 
-def get_ordered_subseqs(seq):
-    s_size = len(seq)
-    for i in range(s_size):
-        rest = seq[i:]
-        
 if __name__ == "__main__":
     seq = parse_infile(argv[1])
     assert type(seq) == list, "Seq is not a list"
     assert type(seq[0]) == int, "Seq items are not integers"
-    get_ordered_subseqs(seq)
+    print " ".join(map(str, get_ordered_subseqs(seq)))
+    seq.reverse()
+    rev = get_ordered_subseqs(seq)
+    rev.reverse()
+    print " ".join(map(str, rev))
--- rosalind/python/sign.py
+++ rosalind/python/sign.py
@@ -6,19 +6,18 @@ from sys import argv
 def permute(num):
     array = [int(x) for x in range(1, num+1)]
     permutated = []
-    for i in xrange(2, num+1):
-        first_permute = list(permutations(array, i))
-        for val in first_permute:
-            permutated.append(val)
-            permutated.append(tuple(map(lambda x: x*-1, val)))
-            for i in range(1, len(val)):
-                second_permute = permutations(val,i)
-                for sec_val in second_permute:
-                    adjustable_val = list(val)
-                    for sep_val in sec_val:
-                        idx = val.index(sep_val)
-                        adjustable_val[idx] = val[idx] * -1
-                    permutated.append(adjustable_val)                    
+    first_permute = list(permutations(array, num))
+    for val in first_permute:
+        permutated.append(val)
+        permutated.append(tuple(map(lambda x: x*-1, val)))
+        for i in range(1, len(val)):
+            second_permute = permutations(val,i)
+            for sec_val in second_permute:
+                adjustable_val = list(val)
+                for sep_val in sec_val:
+                    idx = val.index(sep_val)
+                    adjustable_val[idx] = val[idx] * -1
+                permutated.append(adjustable_val)                    
                
     return permutated
            
@@ -26,9 +25,14 @@ def permute(num):
 if __name__ == "__main__":
     sample = int(argv[1])
     permutated = permute(sample)
-    print(len(permutated))
 
-    for val in permutated:
+
+    import itertools
+    permutated.sort()
+    new_list = list(permutated for permutated,_ in itertools.groupby(permutated)) 
+
+    print(len(new_list))
+    for val in new_list:
         print(" ".join(map(str, val)))
 
 
